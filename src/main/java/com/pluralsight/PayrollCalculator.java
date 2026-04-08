978965c45fca8debf726d2387d501929ebd7b6d9
keziazahiljohn <keziazahiljohn@gmail.com>
2026-04-08 15:51:28 -0400

    Finished Exercise

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = myScanner.nextLine();
+        float gross;
+
+        System.out.print("How many hours did you work this week? ");
+        float hours = myScanner.nextFloat();
+
+        System.out.print("What is your hourly? ");
+        myScanner.nextLine();
+        float rate = myScanner.nextFloat();
+
+
+        System.out.println("Employee Name: " + name);
+
+        if (hours > 40) {
+            rate = rate + 1.5f;
+            gross = hours * rate;
+        } else {
+            gross = hours * rate;
+        }
+
+        System.out.println("Gross Pay: $" + String.format("%.2f", gross));
+
+
+    }
+
 }
